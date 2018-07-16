773954711a4976f7f533c27a8147026e59743a40
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-16 14:55:47 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
 
-module.exports.run = async (bot, message, args) => {
-  message.channel.send('Fuck Off');
-  
-  
-}
+module.exports = (bot, message, args) => {
+  message.channel.send('Pong...').then((msg) => {
+    msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+  });
+};
 module.exports.help = {
-  name: "test"
+  name: "ping"
 }
