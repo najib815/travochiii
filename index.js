029326d7328af6721136b6f52dc87889d3377ecf
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-16 03:40:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const prefix = ';!';
 
 
 
+exports.run = (bot, msg, args) => {
+  msg.delete();
+  msg.channel.send("Ping?").then(m => m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`) );
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ping',
+  description: 'It... like... pings. Then Pongs. And it\'s not Ping Pong.',
+  usage: 'ping'
+};
+
 bot.on('message', message => {
     let msg = message.content.toUpperCase();
     let sender = message.author;
