1b695da873c4486f0e2d5294e6e68519dd4adf28
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-16 16:37:12 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (bot, message, args) => {
-    msg.channel.send(':watch: | Ping!').then(m => {
-        m.edit(`:watch: | Pong! \`${m.createdTimestamp - msg.createdTimestamp}ms\``);
+    message.channel.send(':watch: | Ping!').then(m => {
+        m.edit(`:watch: | Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``);
     });
 };
 
