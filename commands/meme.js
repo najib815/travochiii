4865d11cbe5339d4f292f0478fa0e8f6522f99ed
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-17 01:43:21 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,12 +1,13 @@
 const subs = ['memes', 'me_irl', 'funny'];
 const sortings = ['new', 'top', 'controversial'];
+const snekfetch = require("snekfetch");
 
 module.exports = {
 	description: 'Sends a random meme',
 	category: 'Fun',
 	cooldown: 5000,
 	run: async function (message) {
-		const response = await this.request(`https://www.reddit.com/r/${subs[Math.floor(Math.random() * subs.length)]}/${sortings[Math.floor(Math.random() * sortings.length)]}.json`),
+		const response = await snekfetch.get(`https://www.reddit.com/r/${subs[Math.floor(Math.random() * subs.length)]}/${sortings[Math.floor(Math.random() * sortings.length)]}.json`),
 			body = JSON.parse(response.body),
 			children = body.data.children,
 			childData = children[Math.floor(Math.random() * children.length)].data,
