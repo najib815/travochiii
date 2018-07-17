b115fd562e690a673c235d4b7edee05098dbb0a4
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-17 14:18:21 +0100

    Create cosplay.js

---
+++ commands/cosplay.js
@@ -0,0 +1,24 @@
+const randomPuppy = require('random-puppy');
+const request = require('snekfetch');
+const fs = require("fs")
+
+exports.run = (bot, message, args) => {
+    if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+
+    const subreddits = [
+        'nsfwcosplay',
+        'cosplayonoff',
+        'cosporn',
+        'cosplayboobs'
+    ]
+    const sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            request.get(url).then(r => {
+                fs.writeFile(`cosplay.jpg`, r.body)
+                message.channel.send(r.body)
+                fs.unlink(`./cosplay.jpg`)
+            })
+        })
+}
