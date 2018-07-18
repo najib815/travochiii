e410900efd5965fdfba882408714abf93455d677
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-18 15:28:23 +0100

    Create streaming.js

---
+++ commands/streaming.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports.run = (bot, message, args) => {
+  if (message.author.id !== ('184761349132386306')) return message.channel.send("Huh.");
+  const status = args.join(' ');
+  if (status.length === 0) {
+    const embed = new Discord.RichEmbed()
+      .setColor("#7289DA")
+      .setDescription(':negative_squared_cross_mark: Name streaming status!');
+    message.channel.send({ embed });
+  }
+
+  else if (status.length !== 0) {
+   bot.user.setPresence({ game: { name: `${status}`, url: 'https://twitch.tv/hellcat', type: 1 } });
+  const embed = new Discord.RichEmbed()
+    .setColor("#7289DA")
+    .setDescription(':white_check_mark: You sucessfully changed streaming status');
+  message.channel.send({ embed });
+}};
