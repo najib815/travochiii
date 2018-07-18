8a6922f44b1433eb0e552a087124c12b0ae298ca
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-18 02:34:17 +0100

    Update feed.js

--- commands/feed.js
+++ commands/feed.js
@@ -6,7 +6,7 @@ module.exports.run = async(bot, message, args) => {
     let {body} = await superagent
     .get(`https://nekos.life/api/v2/img/feed`);
     if (!args[0]) {
-            message.channel.send("```Mention who you want to nom nom UwU !!```")
+            message.channel.send("```Mention who you want to feed UwU !!```")
             return;
         }
     if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
@@ -19,8 +19,12 @@ module.exports.run = async(bot, message, args) => {
     .setFooter("By Trav Bot");
 
     message.channel.send(hentaiEmbed);
+    return;
+        }
+        message.channel.send("```You Can't feed Yourself ...UwU !!```")
+} 
 
-}
+
 
 module.exports.help = {
     name: "feed"
