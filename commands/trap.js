f3b0d7ee224cdb5d15f0acfbc63ed859487327a6
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-19 16:23:52 +0100

    Create trap.js

---
+++ commands/trap.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async(bot, message, args) => {
+    
+    let {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/trap`);
+    if (!message.channel.nsfw) return message.reply(" You must be in a N.S.F.W channel to use this command.");
+  
+    let hentaiEmbed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setImage(body.url)
+    .setColor("RANDOM")
+    .setFooter(`Requested by ${message.author.tag} | Powered by Nekos.life`);
+
+    message.channel.send(hentaiEmbed);
+
+}
+
+module.exports.help = {
+    name: "trap"
+}
