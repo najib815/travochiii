dc6689f0c31e2f1e252c3ef99ea7a149f227a4c8
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-20 21:04:01 +0100

    Update lovetest.js

--- commands/lovetest.js
+++ commands/lovetest.js
@@ -4,7 +4,7 @@ const { get } = require("snekfetch");
  module.exports.run = async (bot, message, args) => {
     if (!message.mentions.members.size) return message.response(undefined, "Ba-Baka! How will I tell you, how much you love someone. If I don't know who!"); 
   
-    const data = await get(`https://love-calculator.p.mashape.com/getPercentage?fname=${message.member.displayName}&sname=${message.mentions.members.first().displayName}`).set("X-Mashape-Key", process.env.MASHAPE);
+    const data = await get(`https://love-calculator.p.mashape.com/getPercentage?fname=${message.member.displayName}&sname=${message.mentions.members.first().displayName}`).set("X-Mashape-Key", undefined);
     const embed = new RichEmbed()
       .setThumbnail("http://images6.fanpop.com/image/answers/3317000/3317487_1375024940496.53res_300_202.jpg")
       .addField("Lover", data.body.fname)
