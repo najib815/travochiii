6c332df8ca70e0addbcd230dd6714bfff37fc357
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-20 22:55:02 +0100

    Create boobs.js

---
+++ commands/boobs.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async(bot, message, args) => {
+    
+    let {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/boobs`);
+    if (!message.channel.nsfw) return message.reply(" You must be in a N.S.F.W channel to use this command.");
+  
+    let hentaiEmbed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle("R u flat ? OwO")
+    .setImage(body.url)
+    .setColor("RANDOM")
+    .setFooter(`Requested by ${message.author.tag} | Powered by Nekos.life`);
+
+    message.channel.send(hentaiEmbed);
+
+}
+
+module.exports.help = {
+    name: "boobs"
+}
