2fcd4bf2a3f8a589def75dfa634ad3aae6141cb2
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-20 20:45:02 +0100

    Update lovetest.js

--- commands/lovetest.js
+++ commands/lovetest.js
@@ -2,6 +2,7 @@
  
  module.exports.run = async (message, args, bot) => {
     if (!message.mentions.members.size) return message.response(undefined, "Ba-Baka! How will I tell you, how much you love someone. If I don't know who!"); 
+  
     const data = await get(`https://love-calculator.p.mashape.com/getPercentage?fname=${message.member.displayName}&sname=${message.mentions.members.first().displayName}`).set("X-Mashape-Key", process.env.MASHAPE);
     const embed = new RichEmbed()
       .setThumbnail("http://images6.fanpop.com/image/answers/3317000/3317487_1375024940496.53res_300_202.jpg")
