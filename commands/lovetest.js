cd97cf7a909a1fc46e0d76be7fc45d998552b9b1
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-20 21:10:27 +0100

    Update lovetest.js

--- commands/lovetest.js
+++ commands/lovetest.js
@@ -1,20 +1,23 @@
 const Discord = require("discord.js");
 const { get } = require("snekfetch");
  
- module.exports.run = async (bot, message, args) => {
-    if (!message.mentions.members.size) return message.response(undefined, "Ba-Baka! How will I tell you, how much you love someone. If I don't know who!"); 
-  
-    const data = await get(`https://love-calculator.p.mashape.com/getPercentage?fname=${message.member.displayName}&sname=${message.mentions.members.first().displayName}`).set("X-Mashape-Key", process.env.TYPICAL);
-    const embed = new RichEmbed()
-      .setThumbnail("http://images6.fanpop.com/image/answers/3317000/3317487_1375024940496.53res_300_202.jpg")
-      .addField("Lover", data.body.fname)
-      .addField("Crush", data.body.sname)
-      .addField("Love Percent", data.body.percentage)
-      .setFooter(data.body.result)
-      .setColor(0xFF0000);
-      
-    message.channel.send({ embed });
-  
+ module.exports.run = async(bot, message, args) {
+    const { body } = await get("https://rra.ram.moe/i/r?type=owo");
+    await message.edit({
+      embed: {
+        "title": "Click here if the image failed to load.",
+        "url": `https://cdn.ram.moe/${body.path.replace("/i/", "")}`,
+        "color": 6192321,
+        "image": {
+          "url": `https://cdn.ram.moe/${body.path.replace("/i/", "")}`
+        },
+        "footer": {
+          "icon_url": message.author.displayAvatarURL({ format: "png", size: 32 }),
+          "text": `Requested by ${message.author.tag}`
+        }
+      }
+    });
+
   }
  module.exports.help = {
     name: "lovetest"
