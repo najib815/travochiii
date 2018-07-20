7ceba6a7efe1aaa4e10f5e0ee153ba04d25d0698
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-20 20:43:21 +0100

    Update lovetest.js

--- commands/lovetest.js
+++ commands/lovetest.js
@@ -1,7 +1,7 @@
  const Discord = require("discord.js");
  
- module.exports.run = async (message, args, bot) { // eslint-disable-line no-unused-vars
-    if (!message.mentions.members.size) return message.response(undefined, "Ba-Baka! How will I tell you, how much you love someone. If I don't know who!"); //Response Can Be Refined 😂
+ module.exports.run = async (message, args, bot) => {
+    if (!message.mentions.members.size) return message.response(undefined, "Ba-Baka! How will I tell you, how much you love someone. If I don't know who!"); 
     const data = await get(`https://love-calculator.p.mashape.com/getPercentage?fname=${message.member.displayName}&sname=${message.mentions.members.first().displayName}`).set("X-Mashape-Key", process.env.MASHAPE);
     const embed = new RichEmbed()
       .setThumbnail("http://images6.fanpop.com/image/answers/3317000/3317487_1375024940496.53res_300_202.jpg")
@@ -12,4 +12,5 @@
       .setColor(0xFF0000);
       
     message.channel.send({ embed });
+  
   }
