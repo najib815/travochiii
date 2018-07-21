a7660463735781c11fd3fc9f691f152e73d5c9c7
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 10:11:30 +0100

    Update gayrate.js

--- commands/gayrate.js
+++ commands/gayrate.js
@@ -2,15 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   
-    const randomnumber = Math.floor(Math.random() * 101);
+    const randomnumber = Math.floor(Math.random() * 100 + 1);
     const embed = new Discord.RichEmbed()
-       .setAuthor(${bot.user.username}, ${bot.user.avatarURL})
-       .setTitle("Scanning...")
-       .setThumbnail(${message.author.avatarURL})
-       .setDescription(`${message.member.user.username} is ${randomnumber}% gay! :gay_pride_flag:`)
-       .setColor(0x2471a3)
-       .setTimestamp()
-       .setFooter(${bot.user.avatarURL});
+       .setColor(`Random`)
+       .setTitle("Gay Rate Machine")
+       .setDescription(`${message.member.user.username} you are ${randomnumber}% gay! :gay_pride_flag:`)
+       .setTimestamp();
     
     
     message.channel.send({embed});
