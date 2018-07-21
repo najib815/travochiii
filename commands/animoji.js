710783fce4b92d366e7a98de34d4ede12715fc06
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 07:38:30 +0100

    Create animoji.js

---
+++ commands/animoji.js
@@ -0,0 +1,37 @@
+exports.exec = (bot, message, args) => {
+  if (!args.name) {
+    /**
+     * The command was ran with invalid parameters.
+     * @fires commandUsage
+     */
+    return bot.emit('commandUsage', message, this.help);
+  }
+
+  let emoji = message.guild.emojis.find('name', args.name);
+
+  if (emoji) {
+    message.channel.send({
+      files: [ emoji.url ]
+    }).catch(e => {
+      bot.log.error(e);
+    });
+  }
+};
+
+exports.config = {
+  aliases: [ 'animote' ],
+  enabled: true,
+  argsDefinitions: [
+    { name: 'name', type: String, defaultOption: true }
+  ]
+};
+
+exports.help = {
+  name: 'animoji',
+  description: 'Sends a large version of the specified animated emoji of your Discord server. And Nitro isn\'t required.',
+  botPermission: '',
+  userTextPermission: 'ADD_REACTIONS',
+  userVoicePermission: '',
+  usage: 'animoji <ANIMATED_EMOJI_NAME>',
+  example: [ 'animoji PartyWumpus' ]
+};
