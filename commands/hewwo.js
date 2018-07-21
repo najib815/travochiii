15d939372a756b2b03d701daea71482c9ec1574d
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 18:44:38 +0100

    Update hewwo.js

--- commands/hewwo.js
+++ commands/hewwo.js
@@ -1,11 +1,9 @@
-  exports.run = async (bot, message, args, level) => {
-    if (args.length < 1) return Promise.reject(new this.client.methods.errors.UsageError("You need to give the bot a message to send.", message));
-    return Promise.resolve();
-  }
 
-  async run(message, args) {
+module.exports.run = async(bot, message, args) => {
     await message.channel.send(await this.client.idiotAPI.owoify(args.join(" ")));
   }
 
 
-module.exports = Hewwo;
+module.exports.help = {
+    name: "hewwo"
+}
