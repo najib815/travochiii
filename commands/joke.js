328fcea7d6445cc6c39acba34a0fd1baf4cb7a3a
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 16:39:22 +0100

    Create joke.js

---
+++ commands/joke.js
@@ -0,0 +1,15 @@
+const { get } = require("snekfetch");
+const Discord = require("discord.js");
+
+  exports.run = async(bot, message, args) {
+    const { text } = await get("https://getpuns.herokuapp.com/api/random");
+    const embed = new Discord.RichEmbed()
+      .setThumbnail("https://cdn.discordapp.com/emojis/257279894885498890.png")
+      .setDescription(`_${JSON.parse(text).Pun}_`)
+      .setColor(6192321);
+
+    await message.channel.send({ embed });
+  }
+}
+
+module.exports = PunJoke;
