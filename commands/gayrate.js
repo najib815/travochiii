9affd023924a39a67507058b2e847c21e56610dd
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 10:19:02 +0100

    Update gayrate.js

--- commands/gayrate.js
+++ commands/gayrate.js
@@ -2,14 +2,33 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   
-    const randomnumber = Math.floor(Math.random() * 100 + 1);
-    const embed = new Discord.RichEmbed()
+  const randomnumber = Math.floor(Math.random() * 100 + 1);
+  
+  let henEmbed = new Discord.RichEmbed()
        .setColor(`Random`)
        .setTitle("Gay Rate Machine")
        .setDescription(`${message.member.user.username} you are ${randomnumber}% gay! :gay_pride_flag:`)
        .setTimestamp();
-    
-    
-    message.channel.send({embed});
+  
+    if (!args[0]) {
+            message.channel.send({henEmbed})
+            return;
+        }
+    if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
+  
+    let hentaiEmbed = new Discord.RichEmbed()
+       .setColor(`Random`)
+       .setTitle("Gay Rate Machine")
+       .setDescription(`${message.mentions.members.first().user.username} is ${randomnumber}% gay! :gay_pride_flag:`)
+       .setTimestamp();
+    message.channel.send(hentaiEmbed);
+    return;
+        }
+        message.channel.send({henEmbed})
+} 
 
-};
+
+
+module.exports.help = {
+    name: "gayrate"
+}
