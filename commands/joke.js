48267506e686dcfb2abf9ad7883db4fee920356f
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 18:54:16 +0100

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
     const { text } = await get("https://getpuns.herokuapp.com/api/random");
     const embed = new Discord.RichEmbed()
       .setThumbnail("https://cdn.discordapp.com/emojis/257279894885498890.png")
-      .setDescription(`_${JSON.parse(text).Pun}_`)
+      .setDescription(`_${(text).Pun}_`)
       .setColor(6192321);
 
     await message.channel.send({ embed });
