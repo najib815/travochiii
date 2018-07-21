3589d56014d3d69032ead81d2d6a75181cb665dc
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 07:55:24 +0100

    Update trump.js

--- commands/trump.js
+++ commands/trump.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-exports.run = (bot, message) => {
+module.exports.run = async(bot, message, args) => {
     let finalImage = `https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` + message.content.slice(7).toUpperCase() + `.gif`;
     if (!message.content.slice(7)) {
         return message.reply('specify a thing that trump will make illegal');
@@ -11,7 +11,7 @@ exports.run = (bot, message) => {
   Const embed = new Discord.RichEmbed()
   .setAuthor("Trump", "https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/7d/a4/55/7da455f6-b1f7-be48-7649-bbf90c1a0be1/source/256x256bb.jpg")
   .setImage(finalImage);
-  message.channel.send({embed});
+   message.channel.send({embed});
     //message.channel.send(`https://storage.googleapis.com/is-now-illegal.appspot.com/gifs/` + message.content.slice(7).toUpperCase() + `.gif`);
 };
 module.exports.help = {
