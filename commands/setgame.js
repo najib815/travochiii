82dd05ee721477ddda2b1f3d60c22926eb2b2607
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-21 08:36:11 +0100

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -10,11 +10,11 @@ module.exports = {
         try {
             const game = args.join(" ").trim();
             if (game) {
-                bot.user.setGame(game).catch(e => message.err(e, "while setting game"));
-                message.edit(`Successfully set game to \`${game}\`!`);
+                bot.user.setActivity(game).catch(e => message.err(e, "while setting game"));
+                message.channel.send(`:white_check_mark:Successfully set game to \`${game}\`!`);
             } else {
-                bot.user.setGame(null).catch(e => message.err(e, "while resetting game"));
-                message.edit("Successfully reset game!");
+                bot.user.setActivity(null).catch(e => message.err(e, "while resetting game"));
+                message.channel.send(":arrows_counterclockwise:Successfully reset game!");
             }
         } catch (e) {
             message.err(e, "while changing game");
