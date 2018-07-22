424565e7fa9057eaecadb16c46b7eb9cbda0411d
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-22 21:19:20 +0100

    Update fml.js

--- commands/fml.js
+++ commands/fml.js
@@ -8,12 +8,13 @@ module.exports.run = async(bot, message, args) => {
     const article = root.querySelector(".block a");
     const downdoot = root.querySelector(".vote-down");
     const updoot = root.querySelector(".vote-up");
+    const disc = ${article.childNodes[0].text};
     const embed = new Discord.RichEmbed()
       .setTitle("Fuck my Life, Random Edition!")
       .setColor(165868)
       .setThumbnail("http://i.imgur.com/5cMj0fw.png")
       .setFooter(`Requested by: ${message.member.displayName} | Powered By fmylife.com`)
-      .setDescription(`_${article.childNodes[0].text}\n\n_`)
+      .setDescription(`_disc\n\n_`)
       .addField("I agree, your life sucks", updoot.childNodes[0].text, true)
       .addField("You deserved it:", downdoot.childNodes[0].text, true);
     if (article.childNodes[0].text.length < 5) throw new this.client.methods.errors.APIError("Today, something went wrong, so you'll have to try again in a few moments. FML", message);
