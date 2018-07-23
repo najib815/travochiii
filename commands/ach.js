a5c25f35043b5e3e5741d2ead3cfee9a35c55959
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 07:17:00 +0100

    Delete ach.js

--- commands/ach.js
+++
@@ -1,6 +0,0 @@
-const idiotAPI = require("idiotic-api");
-
-exports.run = async(bot, message, args) => {
-
-await message.channel.send({ files: [{ attachment: await bot.idiotAPI.achievement(message.author.displayAvatarURL({format:"png", size:32}), args.join(" ")), name: "achievement.png" }] });
-}
