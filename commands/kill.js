5c7dd1a33699a1fc9ee34efdfe2460047f63e6bc
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 14:57:43 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -4,10 +4,10 @@ module.exports.run = (bot, message, args) => {
 
 
 	if (args.join(' ').includes('me')) {
-		return bot.reply('no you can do it yourself. Please tag someone else to kill.', message)
+		return message.channel.send('no you can do it yourself. Please tag someone else to kill.', message)
 	}
 	if (!message.mentions[0]) {
-		return bot.reply('please tag someone to kill.', message)
+		return message.channel.send('please tag someone to kill.', message)
 	}
 	message.channel.send(bot.randomInArray(kill)
 		.replace(/\$mention/g, message.mentions[0].username)
