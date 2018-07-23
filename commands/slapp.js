d36e93ca9b0da54db407f83c894e9d9527e7a5a8
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 07:03:45 +0100

    Update slapp.js

--- commands/slapp.js
+++ commands/slapp.js
@@ -4,6 +4,6 @@ const { MessageAttachment } = require("discord.js");
 
 exports.run = async(bot, message, args) => {
 
-   await message.channel.send(new MessageAttachment(await this.bot.idiotAPI.confused(message.author.displayAvatarURL({ format:"png", size:128 }), message.guild.members.random().user.displayAvatarURL({ format: "png", size: 128 })), "confused.png"));
+   await message.channel.send(new MessageAttachment(await bot.idiotAPI.confused(message.author.displayAvatarURL({ format:"png", size:128 }), message.guild.members.random().user.displayAvatarURL({ format: "png", size: 128 })), "confused.png"));
    await message.delete();
 }
