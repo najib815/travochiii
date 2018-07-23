7e197751eeb6d9ff5b97456c84c7b886d90eb49d
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 18:57:28 +0100

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -9,15 +9,30 @@ let chosting = ("TownHosting by Angelo.");
 
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
-	let embed = new Discord.RichEmbed()
-        .setColor("#9B59B6")
-        .setThumbnail(bicon)
-        .addField("Makers:", ccreator)
-        .addField("Idea's:", ccommunity)
-        .addField(`[Click here for more](${flink})`)
+	//let embed = new Discord.RichEmbed()
+        //.setColor("#9B59B6")
+        //.setThumbnail(bicon)
+        //.addField("Makers:", ccreator)
+        //.addField("Idea's:", ccommunity)
+        //.addField(`[Click here for more](${flink})`)
 
 
-        message.channel.send({embed: embed});
+        message.channel.send({embed: {
+	title: "__**${bot.user.tag}** CREDITS__",
+	color: 3447003,
+	fields: [{
+        name: "Makers",
+        value: "${ccreator}"
+      },
+      {
+        name: "Idea's",
+        value: "Travolta (Facebook)[https://www.facebook.com/travolta.youssef.1]"
+      },
+	      {
+        name: "Contact Us",
+        value: "(For More Contact Us In)[https://www.facebook.com/travolta.youssef.1]      
+      },
+	}});
 }
 
 module.exports.help = {
