6280881a909d2c46e3ff28bf8e85e9a7329af249
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 15:09:20 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -2,8 +2,8 @@
 module.exports.run = (bot, message, args) => {
 
 	message.channel.send(`${message.author.username} has died.`).then(message => {
-        setTimeout(() => { message.edit("Respawning..."); }, 1000);
-        setTimeout(() => { message.edit(`Revival complete. Welcome back, ${message.author.username}`); }, 1000);
+        setTimeout(() => { message.edit("Respawning..."); }, 3000);
+        setTimeout(() => { message.edit(`Revival complete. Welcome back, ${message.author.username}`); }, 9000);
      });
 	
 }
