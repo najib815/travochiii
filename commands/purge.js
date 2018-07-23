7be9aa606052833802ce276ea5a24a04f3d2ff4d
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 05:55:42 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,12 +1,17 @@
+const Discord = require('discord.js');
+
 exports.run = async(bot, message, args) => {
 
-    const deleteCount = parseInt(args[0], 10);
-    
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete.");
-   
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+            if (!message.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send(':no_entry `I do not have the correct permissions.`').catch(console.error);
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: `Sorry, but you do not have valid permissions! If you beleive this is a error, contact an owner.`");
+            if (bot(args[1])) return message.channel.send(':warning: `Please supply a valid amount of messages to purge`');
+            if (args[1] > 100) return message.channel.send(':warning: `Please supply a number less than 100`');
+            message.channel.bulkDelete(args[1]);
+            var cleanEmbed = new Discord.RichEmbed()            
+            .setAuthor('Clean Embed')
+            .setDescription(`Cleaned **${args[1]}** messages :white_check_mark:`)
+            .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
+            .setColor('#ffffff');
+            message.channel.send(cleanEmbed);
       
 }
