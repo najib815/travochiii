9c67e811343650880f7b368bfb48b762d2a92221
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 07:22:46 +0100

    Update anime.js

--- commands/anime.js
+++ commands/anime.js
@@ -3,48 +3,24 @@ var aq = require('animequote');
 const Kitsu = require('kitsu.js');
 const kitsu = new Kitsu();
 
-module.exports.run = async(message, args) => {
-        const search = message.content.slice(/\s+/g).split(1).join(" ");
-
-        if (!search) {
-
-            kitsu.searchAnime(aq().quoteanime).then(result => {
-
-                var anime = result[0]
-
-                var embed = new Discord.MessageEmbed()
-                    .setColor('#FF9D6E')
-                    .setAuthor(`${anime.titles.english} | ${anime.showType}`, anime.posterImage.original)
-                    .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
-                    .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
-                    .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
-                    .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
-                    .setImage(anime.posterImage.original);
-                return message.channel.send(`Try watching **${anime.titles.english}**!`, { embed: embed });
-            })
-
-        } else {
-            const search = message.content.split(/\s+/g).slice(1).join(" ");
-
-            kitsu.searchAnime(search).then(result => {
-                if (result.length === 0) {
-                    return message.channel.send(`No results found for **${search}**!`);
-                }
-
-                var anime = result[0]
-
-                var embed = new Discord.MessageEmbed()
-                    .setColor('#FF9D6E')
-                    .setAuthor(`${anime.titles.english ? anime.titles.english : search} | ${anime.showType}`, anime.posterImage.original)
-                    .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
-                    .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
-                    .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
-                    .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
-                    .setImage(anime.posterImage.original);
-                return message.channel.send({ embed });
-            }).catch(err => {
-                console.log(err)
-                return message.channel.send(`No results found for **${search}**!`);
-            });
-        }
+module.exports.run = async(message, args, bot) => {
+    function filter(msg) {
+      if (msg.author.id !== message.author.id) return false;
+      return ["1", "2", "3", "4", "5"].includes(msg.content);
     }
+    if (args.length < 1) return message.reply("You must add an anime to search for");
+    let msg = await message.channel.send("*Fetching information from Kitsu!*");
+    try {
+      const { data } = await kitsu.fetch("anime", { filter: { text: args.join("-") } });
+      if (data.length < 1) throw new bot.methods.errors.AnimeError("No result found");
+      msg = await msg.edit(`Okay, I found 5 possible matches!~ Which one do you want to see~\n(Just write the number, the one you want to see! Cancels in 60 seconds.)${this.makeTitles(data)}`);
+      const collected = await message.channel.awaitMessages(filter, { max: 20, maxProcessed: 1, time: 60000, errors: ["time"] });
+      const returnMessage = collected.first();
+      const index = Number(returnMessage.content) - 1;
+      if (message.channel.permissionsFor(bot.user).has("MANAGE_MESSAGES")) await returnMessage.delete(); 
+      await msg.edit(`**Title JP:** ${data[index].titles.en_jp}\n**Title English:** ${data[index].titles.en}\n**Type:** ${data[index].subtype}\n**Start Date:** ${data[index].startDate}\n**End Date:** ${data[index].endDate || "in Progress"}\n**PopularityRank:** ${data[index].popularityRank}\n**Link:** <https://kitsu.io/anime/${data[index].id}>\n**Synopsis:** ${data[index].synopsis}`);
+    } catch (error) {
+      if (error instanceof Collection) return message.reply("command canceled due to the timer~");
+      throw error;
+    }
+  }
