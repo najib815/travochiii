c6bbaf213f94a773a376f0ec927a1109608a8f27
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 15:11:39 +0100

    Update killme.js

--- commands/killme.js
+++ commands/killme.js
@@ -3,7 +3,7 @@ module.exports.run = (bot, message, args) => {
 
 	message.channel.send(`${message.author.username} has died.`).then(message => {
         setTimeout(() => { message.edit("Respawning..."); }, 3000);
-        setTimeout(() => { message.edit(`Revival complete. Welcome back, ${message.author.username}`); }, 9000);
+        setTimeout(() => { message.edit(`Revival complete. Welcome back.`); }, 9000);
      });
 	
 }
