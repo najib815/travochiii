856a03a8a549458be1daba16ef7b9064821e47e1
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 07:02:53 +0100

    Update slapp.js

--- commands/slapp.js
+++ commands/slapp.js
@@ -4,7 +4,6 @@ const { MessageAttachment } = require("discord.js");
 
 exports.run = async(bot, message, args) => {
 
-   return this.verifyUser(message, message.mentions.users.size === 1 ? message.mentions.users.first().id : message.author.id, { msg: message });
    await message.channel.send(new MessageAttachment(await this.bot.idiotAPI.confused(message.author.displayAvatarURL({ format:"png", size:128 }), message.guild.members.random().user.displayAvatarURL({ format: "png", size: 128 })), "confused.png"));
    await message.delete();
 }
