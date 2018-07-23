2913c2c254eefee3cfc7513a1059970e3c3fe358
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-23 14:55:15 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -1,13 +1,15 @@
+const { kill } = require('../arrays.json');
+
 module.exports.run = (bot, message, args) => {
 
 
 	if (args.join(' ').includes('me')) {
-		return Memer.reply('no you can do it yourself. Please tag someone else to kill.', message)
+		return bot.reply('no you can do it yourself. Please tag someone else to kill.', message)
 	}
 	if (!message.mentions[0]) {
-		return bot.reply('please tag someone to kill.', msg)
+		return bot.reply('please tag someone to kill.', message)
 	}
-	msg.channel.createMessage(bot.randomInArray(kill)
+	message.channel.send(bot.randomInArray(kill)
 		.replace(/\$mention/g, message.mentions[0].username)
 		.replace(/\$author/g, message.author.username))
 }
