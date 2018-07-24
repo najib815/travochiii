cd95b5217a8f89e9636469fcf9c5876faa26c2b8
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-24 23:23:24 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const fs = require("fs");
-const commands = JSON.parse(fs.readFileSync('util/commands.json', 'utf8'));
+const Discord = require("discord.js");
 
-
-module.exports.run = async (bot, message, args, tools, con) => {
-    try {
-        await message.author.send(`Commands: \n\n${bot.commands.map(cmd => `\`${cmd.help.name}\``).join(", ")}`);
-        message.channel.send("Help sent.");
-    } catch (e) {
-        throw e;
-    }
+exports.run = async (bot, message, args, tools, con) => {
+    let help = new Discord.RichEmbed()
+        .setAuthor("List of Commands")
+        .setColor(0x703817)
+        .addField("Category 1", `${bot.commands.filter(cmd => cmd.help.category === 'Category 1').map(cmd => `\`${cmd.help.name}\``).join(", ")}`, true)
+        .addField("Category 2", `${bot.commands.filter(cmd => cmd.help.category === 'Category 2').map(cmd => `\`${cmd.help.name}\``).join(", ")}`, true)
+        .addField("Category 3", `${bot.commands.filter(cmd => cmd.help.category === 'Category 3').map(cmd => `\`${cmd.help.name}\``).join(", ")}`, true);
+    message.channel.send(help)
 }
+
 exports.help = {
-        name: "help"
-    }
+    name: "help",
+    category: "Category 1"
+}
