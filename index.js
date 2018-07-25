1326d6db51d7f21fcfa6cc1744e85c37e9e6edd2
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-25 04:43:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -127,11 +127,26 @@ bot.on('ready', () => {
     console.log('I AM READY !')
     
     bot.user.setStatus('dnd')
-    bot.user.setPresence({ game: { name: `Bot by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
+    bot.user.setPresence({ game: { name: `Dev by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
 })
 bot.on("guildMemberAdd", function(member) {
     let role = member.guild.roles.find("name", "Need Verification");
     member.addRole(role).catch(console.error);
+});
+bot.on('guildCreate', guild => {
+ let defaultChannel = "";
+guild.channels.forEach((channel) => {
+  if(channel.type == "text" && defaultChannel == "") {
+    if(channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+      defaultChannel = channel;
+    }
+  }
+})
+    let embed = new Discord.RichEmbed()
+    .setColor(0x6441A4)
+    .setDescription('Hello, **I'm Calm's Bot** Created and Developed by Sally & Travochii. Thanks for inviting me if you have any questions/suggestions or you face a problem with our bot, we can help you by joining or [Discord Server](https://www.google.com/) or [Contacting](https://www.google.com/).')        
+    .setFooter("Calm's Bot created and developed by 🍯「Hachimitsu」#4965 & 🍭「Satō」#9098");
+defaultChannel.send(embed);
 });
 
 bot.login(process.env.TOKEN);
