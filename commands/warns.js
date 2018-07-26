8f09a7ce1e1d08e294464024d0b6ba8c7e31dfc3
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 07:08:45 +0100

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -1,19 +1,29 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+let warnings = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
-  let warnlevel = warns[wUser.id].warns;
+  let warnedmember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!warnedmember) return message.reply("Couldn't find them yo");
+  
+     if(!warnings[warnedmember.id]) warnings[warnedmember.id] = {
+    warns: 0
+  };
 
-  message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
+  warns[warnedmember.id].warnings++;
+
+  fs.writeFile("./warnings.json", JSON.stringify(warnings), (err) => {
+    if (err) console.log(err)
+});
+let warniings = warnings[warnedmember.id].warnings;
+
+  message.reply(`<@${warnedmember.id}> has ${warniings} warnings.`);
 
 }
 
 module.exports.help = {
-  name: "warnlevel"
+  name: "warns"
 }
