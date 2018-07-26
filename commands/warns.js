6d1ab6ad5b87c9cb8681136343dbf736a27972b0
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 07:17:18 +0100

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -9,15 +9,6 @@ module.exports.run = async (bot, message, args) => {
   let warnedmember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!warnedmember) return message.reply("Couldn't find them yo");
   
-     if(!warnings[warnedmember.id]) warnings[warnedmember.id] = {
-    warnings: 0
-  };
-
-  warnings[warnedmember.id].warnings++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warnings), (err) => {
-    if (err) console.log(err)
-});
 let warniings = warnings[warnedmember.id].warnings;
 
   message.reply(`<@${warnedmember.id}> has ${warniings} warnings.`);
