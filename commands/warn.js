c4e33fc9b06a57b0d83568b0a9803fa2e97fe1f5
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 06:50:02 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 exports.run = async(bot, message, args) => {
 let userCom = bot.users.get(args[0]) || message.mentions.users.last();
@@ -10,15 +13,26 @@ if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorr
      let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
    
+   if(!warns[warnedmember.id]) warns[warnedmember.id] = {
+    warns: 0
+  };
+
+  warns[warnedmember.id].warns++;
+
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+});
+   let warniings = warns[warnedmember.id].warns;
     
-      message.delete().catch(O_o=>{});
-    message.channel.send(`***${warnedmember.user.tag} was warned!***`)
+    message.delete().catch(O_o=>{});
+    message.channel.send(`***${warnedmember.user.tag} was warned!*** \nHe already has ${warniings} Warnings`)
     
     let wembed = new Discord.RichEmbed()
        .setTitle("⚠️ WARNING !!")
        .setThumbnail(avatar)
        .addField('>You have been warned in:', `**${message.guild.name}**\n`)
        .addField('>Warned by :', `**${message.author.username}**\n`)
+       .addField(">Number of Warnings", `**${warniings}** Warnings\n`)
        .addField('>Warning Reason :', `**${reason}**\n`)
        .setColor('#FF0000')
        .setTimestamp();
