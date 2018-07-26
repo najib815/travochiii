a549d124c19d27aacfc99fbb72a9e5bc3d8ba660
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 07:24:07 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+let warns = require("./warnings.json");
 
 exports.run = async(bot, message, args) => {
 let userCom = bot.users.get(args[0]) || message.mentions.users.last();
