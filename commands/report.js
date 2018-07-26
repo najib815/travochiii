2ab3153163d4dad7c90c5c319bac6ac5f08fc78f
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 06:21:00 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -13,13 +13,14 @@ module.exports.run = async (bot, message, args) => {
     .addField("Channel", message.channel)
     .addField("Time", message.createdAt)
     .addField("Reason", rreason);
+    
+    message.channel.send(`${rUser} Successfully Reported`);
 
     let reportschannel = message.guild.channels.find(`name`, "reports");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
 
 
     message.delete().catch(O_o=>{});
-    message.channel.send(`${rUser} Successfully Reported`);
     reportschannel.send(reportEmbed);
 
 }
