57e3fda58c2880db28f87f9bfd523029d4ca4ecb
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-26 16:09:14 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Couldn't find user.");
-    if (!args[0]) return message.channel.send("No reason provided");
     let rreason = args.join(" ").slice(22);
+    if (!rreason) return message.channel.send("No reason provided");
 
     let reportEmbed = new Discord.RichEmbed()
     .setTitle("Reports")
