0fba2fce806fb2190e4ff147e3957884045d2988
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-27 15:05:18 +0100

    Update tst.js

--- commands/tst.js
+++ commands/tst.js
@@ -1,9 +1,9 @@
 exports.run = (bot, message, args) => {
       message.channel.send("TEST 123")
-      //await message.channel.awaitMessages(msg => {
-      if (message.content.startsWith("TEST")) {
+      await message.channel.awaitMessages(msg => {
+      if (msg.content.startsWith("TEST")) {
       
       message.channel.send("OMG IT WORKS !!")
-     // }
+      }
       }
 }
