27144fc3f999f8967f9edee704bc204e8ed48d87
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-28 22:45:05 +0100

    Update burn.js

--- commands/burn.js
+++ commands/burn.js
@@ -2,11 +2,12 @@ const Discord = require('discord.js');
 
 exports.run = async(bot, message) => {
     if (!message.mentions.users.first()) return message.channel.send('Mention someone.');
-    let embed = new Discord.RichEmbed()
+    let hembed = new Discord.RichEmbed()
     .setDescription(`**${message.author.username}** *burned* **${message.mentions.members.first().user.username}**\nYou need some ice for that bud? :snowflake:`)
     .setImage(`https://cdn.discordapp.com/attachments/186920285285384192/262348996784291840/image.gif`)
-    .setColor(`RANDOM`)
-    message.channel.send({embed});
+    .setColor('RANDOM');
+    
+    message.channel.send(hembed);
 }
 
 exports.conf = {
