7cac26e29588d12c98667c2dbc621964f07df43d
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 00:00:15 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+exports.run = async(bot, message, args) => {
+
+message.channel.send("The Help Command is WIP.");
+
+    if (message.args[0] === "test") {
+    
+    message.channel.send("test 1");
+    }
+}
