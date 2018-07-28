db50fa5cebafb0812a20418d0de67183518487c0
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-28 22:24:36 +0100

    Update burn.js

--- commands/burn.js
+++ commands/burn.js
@@ -1,5 +1,5 @@
 exports.run = (message, bot) => {
-    if (!message.mentions.users.first()) return message.channel.send('Mention someone.')
+    if (!message.mentions.members.first()) return message.channel.send('Mention someone.')
     message.channel.send(`**${message.author.username}** *burned* **${message.mentions.users.first().username}**\nYou need some ice for that bud? :snowflake:\nhttps://cdn.discordapp.com/attachments/186920285285384192/262348996784291840/image.gif`)
 }
 
