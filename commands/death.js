92aeee6f035772453de0bf0b17715db3fa9329b1
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-28 22:58:56 +0100

    Create death.js

---
+++ commands/death.js
@@ -0,0 +1,37 @@
+const Canvas = require('canvas');
+const fs = require('fs');
+
+exports.run = async(message, bot) => {
+  let user;
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first().username;
+  } else {
+    return message.channel.send("You need to mention a user.");
+  }
+  let Image = Canvas.Image,
+    canvas = new Canvas(520, 283),
+    ctx = canvas.getContext('2d');
+  fs.readFile('./images/death.png', (err, image) => {
+    if (err) return console.log(err);
+      let img = new Image
+      img.src = image;
+      ctx.drawImage(img, 0, 0, 520, 283);
+      ctx.font = "18px Papyrus";
+      ctx.fillText(user, 275, 80)
+      canvas.toBuffer((err, buff) => {
+        if (err) return console.log(err);
+        message.channel.send("**" + message.author.username + "** *has added*  **" + user + "** *to their death note*")
+        message.channel.sendFile(buff)
+      })
+  })
+}
+
+exports.conf = {
+  userPerm: [],
+  botPerm: ["SEND_MESSAGES"],
+  coolDown: 0,
+  dm: true,
+  category: "Fun",
+  help: "Add a user to your deathnote",
+  args: ""
+}
