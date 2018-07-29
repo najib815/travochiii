f9e52cb05573be6fad84d5503c1944d1eeeb4a42
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 02:29:46 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,10 +7,8 @@ if (!args[0]) {
     let helpembed = new Discord.RichEmbed()
           .setTitle(`**${bot.user.username}** Commands !`)
           .setColor('#00BFFF')
-          .addField("Field For Group name1", `test 123`, true)
-          .addField("Field For Group name2", `test 123`, true)
-          .addField("Field For Group name3", `test 123`, true)
-          .setFooter(`**${bot.user.username}** Created and Developed by Sally & Travochii.`);
+          .setDescription("**The Help Command is WIP.** \nFor more information please contact: \n**🍭「Satō」#9098** or **🍯「Hachimitsu」#4965** ")    
+          .setFooter(`[${bot.user.username}] Created and Developed by Sally & Travochii.`);
             message.channel.send(helpembed)
             return;
 }
