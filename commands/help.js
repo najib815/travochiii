5a810c30820a8392369fec6359aee36e60af2f16
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 02:43:26 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,7 @@ exports.run = async(bot, message, args) => {
 if (!args[0]) {
     let helpembed = new Discord.RichEmbed()
           .setTitle(`**${bot.user.username}** Commands !`)
+          .setImage(`https://vignette.wikia.nocookie.net/supersmashbrosfanon/images/6/63/WIP-logo.png`)
           .setColor('#00BFFF')
           .setDescription("**The Help Command is WIP.** \nFor more information please contact: \n**🍭「Satō」#9098** or **🍯「Hachimitsu」#4965** ")    
           .setFooter(`[${bot.user.username}] Created and Developed by Sally & Travochii.`);
