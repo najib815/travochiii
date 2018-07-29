fb65536c95029923844de6ee23918eb8f6a9f6c0
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 05:55:09 +0100

    Update helptest.js

--- commands/helptest.js
+++ commands/helptest.js
@@ -2,20 +2,19 @@ const Discord = require('discord.js');
 
 
 exports.run = async(bot, message, args) => {
-    
+    let prefix = ";!";
 if (!args[0]) {
     let helpembed = new Discord.RichEmbed()
           .setTitle(`**${bot.user.username}** Commands !`)
           .setColor('#00BFFF')
-          .addField("Field For Group name1", "`test 123`", true)
-          .addField("Field For Group name2", "`test 123`", true)
-          .addField("Field For Group name3", "`test 123`", true)    
+          .addField("Fun Commands", ${prefix} + "`fun`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)` true)
+          .addField("NSFW Commands", "`nsfw`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)` true)
+          .addField("Meme Commands", "`memes`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)` true)   
+          .addField("Coming Soon", "`Coming Soon`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)` true) 
           .setFooter(`[${bot.user.username}] Created and Developed by Sally & Travochii.`);
             message.channel.send(helpembed)
             return;
-}
-
-    if (args[0] === "test") {
+} if (args[0] === "test") {
     
     message.channel.send("test 1");
     }
