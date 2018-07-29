164549b6c20a56eaf5ecdf63836a4d15a1eeb6ce
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 07:35:41 +0100

    Update helptest.js

--- commands/helptest.js
+++ commands/helptest.js
@@ -2,20 +2,27 @@ const Discord = require('discord.js');
 
 
 exports.run = async(bot, message, args) => {
-    let prefix = ";!";
+    
 if (!args[0]) {
     let helpembed = new Discord.RichEmbed()
           .setTitle(`**${bot.user.username}** Commands !`)
           .setColor('#00BFFF')
-          .addField("🕺Fun Commands💯", "`;!help fun` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
-          .addField("😈NSFW Commands🔥", "`;!help nsfw` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
+          .addField("Animals", "`;!help mod` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true) 
+          .addField("🎭Fun Commands🎲", "`;!help fun` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
+          .addField("🤫NSFW Commands🔥", "`;!help nsfw` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
           .addField("😂Meme Commands👌", "`;!help memes` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)   
-          .addField("😴Coming Soon💬", "`Coming Soon` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true) 
+          .addField("😴Mod Commands💬", "`;!help mod` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
+          .addField("📷Image Edit🎨", "`;!help image` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true)
+          .addField("🆗Text Edit🈂", "`;!help text` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true) 
+          .addField("⚒Utility Commands⛏", "`;!help utility` \n[Coming Soon](https://www.facebook.com/travolta.youssef.1)", true) 
           .setFooter(`[${bot.user.username}] Created and Developed by Sally & Travochii.`);
             message.channel.send(helpembed)
             return;
-} if (args[0] === "test") {
-    
-    message.channel.send("test 1");
+} if (args[0] === "fun") {
+    let funembed = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .addField("🎭Fun Commands🎲", "`hug`,`kiss`,`feed`,`fmk`,`cookie`,`nom`,`slap`,`gayrate`,`notice`")
+          .setFooter(`Use ${bot.user.username}'s prefix before every Command \nWe will add more commands as soon as posible.`);
+    message.channel.send(funembed);
     }
 }
