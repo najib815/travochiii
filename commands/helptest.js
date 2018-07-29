3a3dc88a5da2dca31dc0cc4767c65c9030fa0977
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-29 06:06:55 +0100

    Update helptest.js

--- commands/helptest.js
+++ commands/helptest.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 
 exports.run = async(bot, message, args) => {
-    let prefix = ";!";
+    
 if (!args[0]) {
     let helpembed = new Discord.RichEmbed()
           .setTitle(`**${bot.user.username}** Commands !`)
           .setColor('#00BFFF')
-          .addField("Fun Commands", ${prefix}"`fun`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)`, true)
+          .addField("Fun Commands", "`fun`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)`, true)
           .addField("NSFW Commands", "`nsfw`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)`, true)
           .addField("Meme Commands", "`memes`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)`, true)   
           .addField("Coming Soon", "`Coming Soon`", `[Coming Soon](https://www.facebook.com/travolta.youssef.1)`, true) 
