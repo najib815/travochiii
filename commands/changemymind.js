894e1488581cc884b1b8fae91d60dc6e8d37b4fe
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-30 21:28:34 +0100

    Create changemymind.js

---
+++ commands/changemymind.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { get } = require("snekfetch");
+
+module.exports.run = async(bot, message, args) => {
+   let change = args.slice(1).join(' ');
+   if (!change) return message.channel.send("No text provided");
+
+
+    const { body } = await get("https://nekobot.xyz/api/imagegen?type=changemymind&text=${change}");
+    
+    let cembed = nex Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle(`Who Can Change **${message.author.username}**'s Mind ?`)
+       .setImage(body.message)
+       .setTimestamp();
+       message.channel.send(cembed);
+    }
