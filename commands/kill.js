9310fd51a8aeb56472e62bb2ba6aeaed2fb3be37
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-30 23:16:23 +0000

    kill

--- commands/kill.js
+++ commands/kill.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
-    let user = message.mentions.users.first() || message.author;
+    let user = message.mentions.users.first().user.username || message.author.username;
 
     const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&url=${user.displayAvatarURL}`);
    
