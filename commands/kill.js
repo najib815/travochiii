94184ef5cebccca7dba90a576b4ffe5b5e8ce001
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-30 23:11:20 +0000

    kill

--- commands/kill.js
+++ commands/kill.js
@@ -2,38 +2,17 @@ const Discord = require('discord.js');
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
-    let author = (message.author.username);
-    let mention = (message.mentions.members.first().user.username);
-    const { bdy } = await get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${author}`);
+    let user = message.mentions.users.first() || message.author;
     
-    
-    if (!args[0]) {
-        const dembed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setTitle(`**${author}** is about to Suicide.`)
-        .setImage(bdy.message)
-        .setTimestamp();
-        message.channel.send(dembed)
-        return;    
-    }
-    
-  if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
-    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${mention}`);
-      const hembed = new Discord.RichEmbed()
+    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&text=${user.displayAvatarURL}`);
+   
+      let hembed = new Discord.RichEmbed()
           .setColor(`RANDOM`)
-          .setTitle(`**${message.mentions.members.first().user.username}** is about to Suicide.`)
+          .setTitle(`**${user}** is about to Suicide.`)
           .setImage(body.message)
           .setTimestamp()
-      message.channel.send({
-          embed: hembed
-      })
+      message.channel.send(hembed)
       return;
   }
-        const dbembed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setTitle(`**${author}** is about to Suicide.`)
-        .setImage(bdy.message)
-        .setTimestamp();
-        message.channel.send(dembed)
-  message.channel.send(dbembed)
-}
+
+
