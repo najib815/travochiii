a0bca513ae202d48b1267c132cfc0216d3ce2baf
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-30 23:38:29 +0000

    kill

--- commands/kill.js
+++ commands/kill.js
@@ -4,14 +4,15 @@ const { get } = require("snekfetch");
 module.exports.run = async(bot, message, args) => {
     let user = message.mentions.users.first() || message.isMentioned(message.author);
     const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&url=${user.displayAvatarURL}`);
-
-    if(!user) return 
-    let aembed = new Discord.RichEmbed()
+    if (!args[0]) {
+        let aembed = new Discord.RichEmbed()
           .setColor(`RANDOM`)
           .setTitle(`**${message.author.username}** is about to Suicide.`)
           .setImage(body.message)
           .setTimestamp();
-    message.channel.send(aembed);
+     message.channel.send(aembed)
+        return;
+    }
    
     let hembed = new Discord.RichEmbed()
           .setColor(`RANDOM`)
