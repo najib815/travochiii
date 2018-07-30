501bca02fa92154de4e12eeb912e9f69789dcdcc
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-30 21:39:59 +0100

    Update changemymind.js

--- commands/changemymind.js
+++ commands/changemymind.js
@@ -6,7 +6,7 @@ module.exports.run = async(bot, message, args) => {
    if (!change) return message.channel.send("No text provided");
 
 
-    const { body } = await get("https://nekobot.xyz/api/imagegen?type=changemymind&text=${change}");
+    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${change}`);
     
     let cembed = new Discord.RichEmbed()
        .setColor('RANDOM')
