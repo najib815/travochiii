02ea17e1c505871e660a6a65ca5b5182d53147a0
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-31 03:53:51 +0100

    Update phcomment.js

--- commands/phcomment.js
+++ commands/phcomment.js
@@ -2,11 +2,12 @@ const Discord = require('discord.js');
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
+   let user = message.mentions.users.first() || message.author;
    let change = args.join(" ");
    if (!change) return message.channel.send("No text provided");
 
 
-    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=phcomment&image=${message.author.displayAvatarURL}&text=${change}&username=${message.author.username}`);
+    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=phcomment&image=${user.displayAvatarURL}&text=${change}&username=${user.username}`);
     
     let cembed = new Discord.RichEmbed()
        .setColor('RANDOM')
