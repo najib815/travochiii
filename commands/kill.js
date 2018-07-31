617f5bfadb7840f7361dca5c6929eabee678de42
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-31 01:23:18 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
-    let user = message.mentions.users.first() || message.author;
-    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&url=${user.displayAvatarURL}`);
-    
+    let avatar = message.mentions.users.first() || message.author;
+    const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&url=${avatar.displayAvatarURL}`);
+    let user = message.mentions.users.first().user.username || message.author.username;
    
     let hembed = new Discord.RichEmbed()
           .setColor(`RANDOM`)
