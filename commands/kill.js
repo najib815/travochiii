f51b51c4fd77959ee831af00893189c986666b27
najib815 <41246767+najib815@users.noreply.github.com>
2018-07-31 01:24:51 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
-    let avatar = message.mentions.users.first() || message.author;
+    let user = message.mentions.users.first() || message.author;
     const { body } = await get(`https://nekobot.xyz/api/imagegen?type=kms&url=${avatar.displayAvatarURL}`);
-    let user = message.mentions.users.first().user.username || message.author.username;
+   // let user = message.mentions.users.first().user.username || message.author.username;
    
     let hembed = new Discord.RichEmbed()
           .setColor(`RANDOM`)
-          .setTitle(`**${user}** is about to Suicide.`)
+          .setTitle(`**${user.username}** is about to Suicide.`)
           .setImage(body.message)
           .setTimestamp();
       message.channel.send(hembed)
