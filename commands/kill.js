165907996203643063112e21ef4579aa5f7307c0
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 20:22:32 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -6,13 +6,13 @@ module.exports.run = (bot, message, args) => {
          const pokem = kill[poke];
 	
           if (!args[0]) {
-            message.channel.send("no you can do it yourself. Please tag someone else to kill.")
+            message.channel.send("please tag someone to kill.")
             return;
        }
 	if (!message.isMentioned(message.author)) {
-		return message.reply('please tag someone to kill.')
-	}
-	message.channel.send(pokem
+		return message.channel.send(pokem
 		.replace(/\$mention/g, message.mentions.users.first().username)
 		.replace(/\$author/g, message.author.username))
+	}
+	message.reply('no you can do it yourself. Please tag someone else to kill.')
 }
