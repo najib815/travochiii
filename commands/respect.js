7d7acb84db3d5cdd3cc9a342ac265e50db11a062
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 05:15:01 +0100

    Create respect.js

---
+++ commands/respect.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async(bot, message, args) => {
+
+const { respect } = args;
+        if (respect == 'none') {
+            const embed = new Discord.RichEmbed()
+                .setAuthor(`${message.author.username} has paid their respects.`, message.author.displayAvatarURL({ format: 'png' }))
+                .setColor('#4E373B')
+                .setFooter(`Press F to pay your respects.`);
+            message.channel.send({ embed }).then(m => m.react("🇫"));
+
+            return null;
+
+        } else {
+            const embed = new Discord.RichEmbed()
+                .setAuthor(`\u2000`, message.author.displayAvatarURL({ format: 'png' }))
+                .setColor('#4E373B')
+                .setDescription(`${message.author} has paid their respects to ${respect}`)
+                .setFooter(`Press F to pay your respects.`);
+            message.channel.send({ embed }).then(m => m.react("🇫"));
+
+return null;
+}
+}
