297157670eed1c72d0e4f6c32547b9ab07c67ab9
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 03:09:41 +0100

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -3,7 +3,7 @@ const figlet = require('figlet');
 exports.run = async (bot, message, args) => {
   
   if(args.join(' ').length > 14) return message.channel.send('Only 14 characters are admitted!') 
-  if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>')
+  if (!args.join(' ')) return message.channel.send(`Please, provide text to format in **ASCII** ! \n**Usage:** ascii <text>`)
     figlet(args.join(' '), (err, data) => {
       message.channel.send('```' + data + '```')
     })
