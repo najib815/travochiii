8ed6be899bf32b53046fd4b5ebb16f2533a87df9
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 13:30:22 +0000

    yoda

--- commands/yoda.js
+++ commands/yoda.js
@@ -3,6 +3,6 @@ const { get } = require("snekfetch");
 module.exports.run = async(bot, message, args) => {
     if (args.length < 2) return message.channel.send("Invalid command usage, you must supply text for Yoda. Yes.");
 
-    const { text } = await get(`http://yoda-api.appspot.com/api/v1/yodish?text=${encodeURIComponent(args.join(" ").toLowerCase())}`);
-    message.channel.send(JSON.parse(text).yodish);
+    const { body } = await get(`http://yoda-api.appspot.com/api/v1/yodish?text=${encodeURIComponent(args.join(" ").toLowerCase())}`);
+    message.channel.send(body.yodish);
 }
--- commands/yomomma.js
+++ commands/yomomma.js
@@ -1,6 +1,6 @@
 const { get } = require("snekfetch");
 
 module.exports.run = async(bot, message, args) => {
-    const { text } = await get("http://api.yomomma.info/");
-    message.channel.send(JSON.parse(text).joke);
+    const { body } = await get("http://api.yomomma.info/");
+    message.channel.send(body.joke);
 }
