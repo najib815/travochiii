47e3f070304e294b91a82cf05845c245ce4c3c51
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 05:38:03 +0100

    Update respect.js

--- commands/respect.js
+++ commands/respect.js
@@ -6,11 +6,11 @@ module.exports.run = async(bot, message, args) => {
     let meow = message.content.split(" ").slice(1);
     let respect = meow.join(' ');
      if (!respect) {
-            const embed = new Discord.RichEmbed()
+            const rembed = new Discord.RichEmbed()
                 .setAuthor(`${message.author.username} has paid their respects.`, message.author.AvatarURL)
                 .setColor('#4E373B')
                 .setFooter(`Press F to pay your respects.`);
-            message.channel.send({ embed }).then(m => m.react("🇫"));
+            message.channel.send({ rembed }).then(m => m.react("🇫"));
               } 
             const embed = new Discord.RichEmbed()
                 .setAuthor(`\u2000`, message.author.AvatarURL)
