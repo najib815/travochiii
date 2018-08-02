580cab718bc4ee53e96d91fb71fe2a3ea09102de
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 12:52:21 +0000

    fml

--- commands/fml.js
+++ commands/fml.js
@@ -2,19 +2,20 @@ const request = require("snekfetch");
 const HTMLParser = require("fast-html-parser");
 const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
+
     const text = await request.get("http://www.fmylife.com/random");
     const root = HTMLParser.parse(text);
-    const article = ${root.querySelector(".block a")};
-    const downdoot = ${root.querySelector(".vote-down")};
-    const updoot = ${root.querySelector(".vote-up")};
-    const disc = ${article.childNodes[0].text};
+    const article = root.querySelector(".block a");
+    const downdoot = root.querySelector(".vote-down");
+    const updoot = root.querySelector(".vote-up");
+    const disc = article.childNodes[0].text;
     const embed = new Discord.RichEmbed()
       .setTitle("Fuck my Life, Random Edition!")
       .setColor(165868)
       .setThumbnail("http://i.imgur.com/5cMj0fw.png")
       .setFooter(`Requested by: ${message.member.displayName} | Powered By fmylife.com`)
       .setDescription(`_disc\n\n_`)
-      .addField("I agree, your life sucks", ${updoot.childNodes[0].text}, true)
-      .addField("You deserved it:", ${downdoot.childNodes[0].text}, true);
+      .addField("I agree, your life sucks", updoot.childNodes[0].text, true)
+      .addField("You deserved it:", downdoot.childNodes[0].text, true);
     message.channel.send({ embed });
-};
+}
