876afb2c0b9eee48ca335506dfe41542ca21a24f
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 03:16:26 +0100

    Create moe.js

---
+++ commands/moe.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+
+module.exports.run = (bot, message) => {
+        randomPuppy('awwnime')
+            .then(url => {
+                const embed = new Discord.MessageEmbed()
+                    .setFooter(`awwnime`)
+                    .setDescription(`[Image URL](${url})`)
+                    .setImage(url)
+                    .setColor('#A187E0')
+                return message.channel.send({ embed })
+}
