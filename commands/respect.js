cfd16504f10cac68d852b470d5a2a2810d141e90
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 06:03:05 +0100

    Update respect.js

--- commands/respect.js
+++ commands/respect.js
@@ -2,22 +2,25 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async(bot, message, args) => {
-
-    let meow = message.content.split(" ").slice(1);
-    let respect = meow.join(' ');
-     if (!respect) {
-            const rembed = new Discord.RichEmbed()
-                .setAuthor(`${message.author.username} has paid their respects.`, `message.author.iconURL`)
+const { respect } = args;
+        if (respect == 'none') {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`${message.author.username} has paid their respects.`, message.author.AvatarURL({ format: 'png' }))
                 .setColor('#4E373B')
                 .setFooter(`Press F to pay your respects.`);
-            message.channel.send({ rembed }).then(m => m.react("🇫"))
-         return;
-              } 
-            const embed = new Discord.RichEmbed()
-                .setAuthor(`\u2000`, `message.author.iconURL`)
+            message.channel.send({ embed }).then(m => m.react("🇫"));
+
+            return null;
+
+        } else {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`\u2000`, message.author.AvatarURL({ format: 'png' }))
                 .setColor('#4E373B')
                 .setDescription(`${message.author} has paid their respects to ${respect}`)
                 .setFooter(`Press F to pay your respects.`);
             message.channel.send({ embed }).then(m => m.react("🇫"));
+
+            return null;
+}
 
 }
