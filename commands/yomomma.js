06dad3c987e101e2eb17b0eaf110ae481447b2c7
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 13:17:06 +0000

    yomomma

---
+++ commands/yomomma.js
@@ -0,0 +1,6 @@
+const { get } = require("snekfetch");
+
+module.exports.run = async(bot, message, args) => {
+    const { text } = await get("http://api.yomomma.info/");
+    message.channel.send(`_${JSON.parse(text).joke}_`);
+}
