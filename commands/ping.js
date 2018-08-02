50bd7aecce5ab795eae0b5092b963e061fe38c22
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-02 02:41:06 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,23 @@
-exports.run = (bot, message, args) => {
-    message.channel.send(':watch: | Ping!').then(m => {
-        m.edit(`:watch: | Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``);
+const Discord = require('discord.js')
+
+exports.run = async(bot, message, args) => {
+
+    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
+    let diff = (Date.now() - start); 
+    let API = (bot.ping).toFixed(2)
+        
+        let embed = new Discord.RichEmbed()
+        .setTitle(`🔔 Pong!`)
+        .setColor(0xff2f2f)
+        .addField("📶 Latency", `${diff}ms`, true)
+        .addField("💻 API", `${API}ms`, true)
+        message.edit(embed);
+      
     });
-};
+
+}
 
 exports.help = {
     name: 'ping',
-    usage: 'ping',
-    description: 'Pings the bot to check its connection speed.'
-};
+    category: 'INFO'
+}
