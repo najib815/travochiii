cd2f8a3ebb38e98c78dc2b83407c8c328b28e308
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-03 21:35:34 +0100

    Create net.js

---
+++ commands/net.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+exports.run = async(bot, message, args) => {
+
+  var user = message.mentions.users.first();
+  if (!user) {
+var whatif = [`in the head`, `in the face`, `in the foot`, `in the knee`]
+var character = ["Sportacus", "Robbie Rotten", "Pie", "Discord", "Hitler", "Grandayy", "Michael Stevens", "Annoying Orange", "PewDiePie", "Keemstar", "Scarce", "Valivia", "Michael Rosen", "RazorBlade", "John"]
+var action = [`misses`, `captures ${character[~~(Math.random() * character.length)]}** instead`, `hits **${character[~~(Math.random() * character.length)]}** ${whatif[~~(Math.random() * whatif.length)]} instead`, `kills **${character[~~(Math.random() * character.length)]}** instead`, `shoots **${character[~~(Math.random() * character.length)]}** ${whatif[~~(Math.random() * whatif.length)]} instead`]
+var response = [`**${message.author.username}** throws the net at **${character[~~(Math.random() * character.length)]}** but ${action[~~(Math.random() * action.length)]}.`]
+const embed1 = new Discord.RichEmbed()
+.setColor(0xFFB200)
+.setTimestamp()
+.addField('Net', response[~~(Math.random() * response.length)])
+.setFooter(`Thrown by ${message.author.username}`)
+return message.channel.send(embed1);
+}
+var whatif = [`in the head`, `in the face`, `in the foot`, `in the knee`, `in the dick`, `in the deadly lazer`]
+var character = ["Sportacus", "Robbie Rotten", "Pie", "Discord", "Hitler", "Grandayy", "Michael Stevens", "Annoying Orange", "PewDiePie", "Keemstar", "Scarce", "Valivia", `${user.username}`, "Michael Rosen", "RazorBlade", "John"]
+var action = [`misses`, `captures **${character[~~(Math.random() * character.length)]}** instead`, `hits **${character[~~(Math.random() * character.length)]}** ${whatif[~~(Math.random() * whatif.length)]} instead`, `kills **${character[~~(Math.random() * character.length)]}** instead`, `shoots **${character[~~(Math.random() * character.length)]}** ${whatif[~~(Math.random() * whatif.length)]} instead`]
+var response = [`**${message.author.username}** throws the net at **${user.username}** but ${action[~~(Math.random() * action.length)]}.`]
+const embed2 = new Discord.RichEmbed()
+.setColor(0xFFB200)
+.setTimestamp()
+.addField('Net', response[~~(Math.random() * response.length)])
+.setFooter(`Thrown by ${message.author.username}`)
+message.channel.send(embed2);
+}
