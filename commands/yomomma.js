bc4213d3d244343bafdb984049dfa1ff3a0b7cea
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 21:37:46 +0100

    Delete yomomma.js

--- commands/yomomma.js
+++
@@ -1,6 +0,0 @@
-const { get } = require("snekfetch");
-
-module.exports.run = async(bot, message, args) => {
-    const { body } = await get("http://api.yomomma.info/");
-    message.channel.send(body.joke);
-}
