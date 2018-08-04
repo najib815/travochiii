7d9d9e7174933e08148efcf5eb8724433612cc97
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 04:09:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -90,12 +90,14 @@ bot.on('message', message => {
 })
 bot.on('ready', () => {
     console.log('I AM READY !')
-    bot.user.setActivity(games[Math.floor(Math.random() * (games.length))]).then(() => {
-        setTimeout(() => 10000);
+    
+    //bot.user.setActivity(games[Math.floor(Math.random() * (games.length))]).then(() => {
+       // setTimeout(() => 10000);
         
-    })
+   // })
+    
     bot.user.setStatus('dnd')
-    //bot.user.setPresence({ game: { name: `Dev by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
+    bot.user.setPresence({ game: { name: `Dev by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
 })
 bot.on("guildMemberAdd", function(member) {
     let role = member.guild.roles.find("name", "Need Verification");
