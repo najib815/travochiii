03fac6dd32c9e7bd2231c0c88aadd14587ad2909
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 03:49:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ bot.on('ready', () => {
     console.log('I AM READY !')
     bot.user.setActivity(games[Math.floor(Math.random() * (games.length))]).then(() => {
         setTimeout(() => {db.checkServers(bot)}, 10000);
-        }, 2000)
+        
     })
     bot.user.setStatus('dnd')
     //bot.user.setPresence({ game: { name: `Dev by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
