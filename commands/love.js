91a330217de5dced87c9807ae98ff76a0b3a407d
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 21:23:27 +0100

    Create love.js

---
+++ commands/love.js
@@ -0,0 +1,6 @@
+exports.run = async(bot, message, args) => {
+
+      message.channel.send(':cherry_blossom: Hey, you have to know that someone, (somewhere,somehow), loves you.:revolving_hearts:');
+
+
+}
