24d1af121dd5ce4541e9c55046431e7487f313fa
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 21:27:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,60 +32,6 @@ bot.on('message', message => {
     } finally {
         console.log(`${message.author.username} ran the command: ${cmd}`);
     }
-    
-    
-    if (msg.startsWith(prefix + '8BALL')) {
-      if ( msg.endsWith('?')) {
-      let answers = [
-      'Maybe.', 'Lol no.', 'I really hope so.', 'Not in your wildest dreams.',
-      'There is a good chance.', 'Quite likely.', 'I think so.', 'I hope not.',
-      'I hope so.', 'Wtf no!', 'Fuhgeddaboudit.', 'Ahaha! Really?!?', 'Pfft.',
-      'Sorry, bby.', 'fuck, yes.', 'Hell to the no.', 'ehhhhhh, i dont know.',
-      'The future is uncertain.', 'I would rather not say.', 'Who cares?',
-      'Possibly.', 'Never, ever, ever.', 'There is a small chance.', 'Yes!'];
-      let answer = Math.floor((Math.random() * answers.length));
-          message.channel.send(answers[answer])
-     } else {
-      message.channel.send('Is that a question?')
-     }
-}
-
-    if (msg.startsWith(prefix + 'COOKIE')) {
-        let cookie = [
-          "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/f/f3/DANCING_COOKIE.gif",
-          "https://media.giphy.com/media/sBs1ITniVp7tm/giphy.gif",
-          "https://i.gifer.com/21aK.gif",
-          "https://steamusercontent-a.akamaihd.net/ugc/764896935328934464/D7ABB5A6C58BC7A3E20548EC389583BBAC8B0795/",
-          "https://orig00.deviantart.net/3742/f/2017/175/2/0/tumblr_orplg1cwxr1u1wqbzo1_540_by_ari_6-dbdxpp7.gif",
-          "https://pa1.narvii.com/6451/ab46c5ffc6341ab41b8cee643b82fa0082cd3dc6_hq.gif",
-          "https://media0.giphy.com/media/EKUvB9uFnm2Xe/giphy.gif",
-          
-          
-        ]
-        let hugresult = Math.floor((Math.random() * cookie.length));
-        if (!args[0]) {
-            message.channel.send("```You have to mention someone first !```")
-            return;
-        }
-        if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
-            const hembed = new Discord.RichEmbed()
-                .setColor(`RANDOM`)
-                .setTitle(`${message.author.username} gave ${message.mentions.members.first().user.username} a Sweet COOKIE :cookie: !!! `)
-                .setImage(cookie[hugresult])
-                .setTimestamp()
-            message.channel.send({
-                embed: hembed
-            })
-            return;
-        }
-        message.channel.send("```So Sad ! You need someone to give it to you :( ```")
-    } 
-
-
-    if (msg === prefix + 'LOVE') {
-        
-        message.channel.send(':cherry_blossom: Hey, you have to know that someone, (somewhere,somehow), loves you.:revolving_hearts:');
-    }
    
 })
 bot.on('ready', () => {
@@ -93,7 +39,7 @@ bot.on('ready', () => {
     bot.setInterval(() => {
 	const activity = games[Math.floor(Math.random() * games.length)];
 	bot.user.setActivity(activity);
-}, 1800000);
+}, 800000);
     bot.user.setStatus('dnd')
     //bot.user.setPresence({ game: { name: `Dev by Sally & Travo`, url: 'https://www.twitch.tv/najib815', type: 1 } });
 })
