fc6efb3f6a4a1461cfbfcfb5d08be8ac32573413
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-04 23:37:50 +0100

    Update respect.js

--- commands/respect.js
+++ commands/respect.js
@@ -4,13 +4,13 @@ const superagent = require("superagent");
 module.exports.run = async(bot, message, args) => {
         
         let respect = args.join(" ");
-        if (!respect) {
-            let embed = new Discord.RichEmbed()
-                .setTitle(`**${message.author.username}** has paid their respects.`)
-                .setColor('#4E373B')
-                .setFooter(`Press F to pay your respects.`);
-            return message.channel.send({ embed }).then(m => m.react("🇫"));
-        }
+        if (!respect) //{
+            //let embed = new Discord.RichEmbed()
+                //.setTitle(`**${message.author.username}** has paid their respects.`)
+                //.setColor('#4E373B')
+                //.setFooter(`Press F to pay your respects.`);
+            return message.channel.send("test").then(m => m.react("🇫"));
+        //}
             let hembed = new Discord.RichEmbed()
                 .setTitle(`\u2000`)
                 .setColor('#4E373B')
