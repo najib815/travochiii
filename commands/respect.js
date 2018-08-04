443c605aefd0b0333d8d2857661adf90ad752cc2
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-05 00:55:27 +0100

    Update respect.js

--- commands/respect.js
+++ commands/respect.js
@@ -12,7 +12,7 @@ module.exports.run = async(bot, message, args) => {
             let hembed = new Discord.RichEmbed()
                 .setTitle(`\u2000`)
                 .setColor('RANDOM')
-                .addField(`**${message.author}** has paid their respects to **${text}**`)
+                .addField(`**${message.author.username}** has paid their respects to **${text}**`)
                 .setFooter(`Press F to pay your respects.`);
                message.channel.send(hembed).then(m => m.react("🇫"));
 }
