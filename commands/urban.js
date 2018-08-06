2ec78419bc20500a9394821c5b2d3e0d4e1ac372
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-06 13:11:22 +0000

    done

---
+++ commands/urban.js
@@ -0,0 +1,34 @@
+const urban = require('relevant-urban');
+const Discord = require('discord.js');
+
+
+exports.run = async (bot, message, args) => {
+
+
+  if (!args[0]) return message.channel.send(`***Please specify some text!***`);
+
+
+
+  let res = await urban(args.join(' ')).catch(e => { 
+
+    return message.channel.send('***Sorry, that word was not found!***');
+  });
+
+
+  const embed = new Discord.MessageEmbed()
+    .setColor('RANDOM') 
+    .setTitle(res.word)
+    .setUrl(res.urbanURL) 
+    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
+    .setField('Author', res.author, true)
+    .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`) 
+
+  if (res.tags.length > 0 && res.tags.join(' ').length < 1024) {
+
+    embed.addField('Tags', res.tags.join(', '), true)
+  }
+
+
+  message.channel.send(embed);
+
+}
--- index.js
+++ index.js
@@ -59,10 +59,10 @@ guild.channels.forEach((channel) => {
     let embed = new Discord.RichEmbed()
     .setColor(0x6441A4)
     .setDescription(`Hello, I'm **${bot.user.username}** Created and Developed by Sally & Travochii. Thanks for inviting me. \nIf you have any questions/suggestions or you need help feel free to join our [Discord Server](https://discord.gg/bP3hZJ4) \nOr add us on Discord : `)
-    .addField("👩‍ Sally 🌸", '**S Marokkanskiy#4965**', true)
-    .addField("👨‍ Travo 💛", '**N Travolta#9098**')
-    .addField("Usage :", `Prefix "${prefix}"`, true)   
-    .addField("Commands :", `${prefix}help for commands list`)     
+    .addField("👩‍ Sally 🌸", '**S Marokkanskiy#4965**')
+    .addField("👨‍ Travo 💛", '**N Travolta#9098**', true)
+    .addField("Usage :", `Prefix "${prefix}"`)   
+    .addField("Commands :", `${prefix}help for commands list`, true)     
     .setFooter("Calm Bot created and developed by S Marokkanskiy#4965 & N Travolta#9098");
 defaultChannel.send(embed);
 });
