1a9c20309c8fab48e5495a2418d6750c90e37010
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-09 00:46:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,36 +20,6 @@ bot.on('message', message => {
           message.channel.send("yes?");
     }
     if (sender.bot) return;
-	
-    let xpAdd = Math.floor(Math.random() * 7) + 8;
-  console.log(xpAdd);
-
-  if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-    };
-  }
-
-
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
-    xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.RichEmbed()
-    .setTitle("Level Up!")
-    .setColor('RRANDOM')
-    .addField("New Level", curlvl + 1);
-
-    message.channel.send(lvlup).then(msg => {msg.delete(15000)});
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
-});	
-	
- 
 
     if (!message.content.startsWith(prefix)) return; 
 
