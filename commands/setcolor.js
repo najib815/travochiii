4e7d95b394be1ff33e98a2f340b3586879b768e8
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-09 01:22:29 +0100

    Create setcolor.js

---
+++ commands/setcolor.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+exports.run = async(bot, message, args) => {
+
+let color = message.content.split(/\s+/g).slice(1).join(" ");
+
+if (!color) {
+            var genColour = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            const embed = new Discord.MessageEmbed()
+                .setColor(genColour)
+                .setImage(`https://dummyimage.com/50/${genColour.slice(1)}/&text=%20`)
+                .setFooter(genColour);
+            return message.channel.send('Here\'s your color!', { embed: embed });
+}
+let colorole = message.guild.createRole({
+                name: 'USER-' + message.author.id ,
+                color: `color`,
+                permissions: []
+            })
+       message.author.addRole(colorole);
+}
