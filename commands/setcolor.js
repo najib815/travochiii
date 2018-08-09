489ca5178b9a8ef638c5d5a170b40ec29df7a1ac
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-09 19:10:57 +0100

    Update setcolor.js

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -11,7 +11,9 @@ if (!color) {
                 .setImage(`https://dummyimage.com/150x70/${genColour.slice(1)}/&text=%20`)
                 .setFooter(genColour);
             return message.channel.send('Here\'s your color!', { embed: embed });
+            message.react("✅");
 }
+            if (message.author.react("✅")) return message.channel.send("Hi !");
             let num = message.guild.roles.find(`name`, "TravochiiBot");
             let colorole = await message.guild.createRole({
                 name: 'CBOT-' + RUser.id,
