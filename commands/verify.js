6411da5fb4fa2fa0adbac17cf648cf2dbe962ad5
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-10 04:06:28 +0100

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -8,13 +8,13 @@ exports.run = async(bot, message, args) => {
             .then(m => m.delete(30000));
     }
     // Mention the user that you want to verify
-    let toverify = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    let toverify = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     // Find the role 'Verified User'
     let verifyrole = message.guild.roles.find(`name`, "Verified User");
     if (!verifyrole) return message.reply('❗️ You have to Creat a roll and name it **Verified User**').then(m => m.delete(5000));
-    if (toverify.roles.exists(`name`, "Verified User")) return message.reply('❌ This User is already verified').then(m => m.delete(5000));
-    // Check if a user has been mentioned.
+        // Check if a user has been mentioned.
     if (!toverify) return message.reply("You need to mention a user.").then(m => m.delete(10000));
+    if (toverify.roles.exists(`name`, "Verified User")) return message.reply('❌ This User is already verified').then(m => m.delete(5000));
     // Add the role to the user if one is mentioned.
     await (toverify.addRole(verifyrole.id));
 
@@ -37,7 +37,7 @@ exports.run = async(bot, message, args) => {
     if (!veriflog) return message.channel.send('❌ Could not find the `Verification User Log Channel.` \nCreat One and name it `log_channel`.');
    
     message.delete().catch(O_o=>{});
-    message.channel.send(`☑ **${toverify.username}** **Successfully Verified**`).then(m => m.delete(15000));
+    message.channel.send(`☑ **${toverify}** **Successfully Verified**`).then(m => m.delete(15000));
     
     
     veriflog.send(verifembed);
