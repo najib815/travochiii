49a976e5d688a4d1742668d921dba532006cdc4d
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-15 02:16:21 +0100

    Create pussy.js

---
+++ commands/pussy.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async(bot, message, args) => {
+    
+    let {body} = await superagent
+    .get(`https://nekobot.xyz/api/image?type=pussy`);
+    if (!message.channel.nsfw) return message.reply(" You must be in a N.S.F.W channel to use this command.");
+  
+    let hentaiEmbed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle("Pussy Pussy Pussy !!")
+    .setImage(body.message)
+    .setColor("RANDOM")
+    .setFooter(`Requested by ${message.author.tag} | CalmBot`);
+
+    message.channel.send(hentaiEmbed);
+
+}
+
+module.exports.help = {
+    name: "boobs"
+}
