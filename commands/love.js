c821ab288e1d0a88eadd20e0787f2e3853715ed0
najib815 <41246767+najib815@users.noreply.github.com>
2018-08-16 08:47:49 +0100

    Delete love.js

--- commands/love.js
+++
@@ -1,6 +0,0 @@
-exports.run = async(bot, message, args) => {
-
-      message.channel.send(':cherry_blossom: Hey, you have to know that someone, (somewhere,somehow), loves you.:revolving_hearts:');
-
-
-}
